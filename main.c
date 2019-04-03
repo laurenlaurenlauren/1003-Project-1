4af285dbdb334e34c4e9581b56e6591f316d98b8
lauren prowse <c3331048@uon.edu.au>
2019-04-03 07:12:05 +0000

    Added some "printf's" and "scanf's" to try read input for task one.

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+char encrypt_rotation (int rotation, r);
 int main () {
+char wowow
 
+printf("enter a text:\n");
+scanf("%s", wowow)
+printf("enter a key:"); 
+scanf("%d", r);
 
 return 0;
 }
