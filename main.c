967503dcccbcb16774c57238aa45c8ca0cc9cfd7
laurenlaurenlauren <49218792+laurenlaurenlauren@users.noreply.github.com>
2019-04-16 13:09:59 +1000

    extended hard coded menu

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-char encrypt_rotation (int rotation, r);
 int main () {
 Printf("select an option");
-  printf("1: encrypt");
-  printf("2: decrypt");
+  printf("1: encrypt rotation\n");
+  printf("2: decrypt rotation\n");
+  printf("3: encrypt substitution\n);
+  printf("4: decrypt substitution\n);
 
 return 0;
 }
