27a25286a6d5850118a1aa68027bdf84e39926d8
laurenlaurenlauren <49218792+laurenlaurenlauren@users.noreply.github.com>
2019-04-18 10:35:49 +1000

    Update main.c

--- main.c
+++ main.c
@@ -29,7 +29,7 @@ void E_rotation (char message[], int shift) {
   int i = 0;
 
   while (message[i] != '\0') {
-    if ((message[i] += shift) >= 65 && (message[i] += shift) <= 90) {
+    if ((message[i] + shift) >= 65 && (message[i] + shift) <= 90) {
       message[i] += (shift);
     } else {
       message[i] += (shift - 25); 
