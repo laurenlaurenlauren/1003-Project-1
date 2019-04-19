0f1d6d1e5bf93804050caa70515a1031cd2bf9b1
laurenlaurenlauren <49218792+laurenlaurenlauren@users.noreply.github.com>
2019-04-19 10:42:33 +1000

    added some comments to keep track

--- main.c
+++ main.c
@@ -12,17 +12,17 @@ int main () {
   printf("1: encrypt rotation\n");
   printf("2: decrypt rotation\n");
   printf("3: encrypt substitution\n");
-  printf("4: decrypt substitution\n");
+  printf("4: decrypt substitution\n"); //hard coded menu
          scanf("%d", &x);
          
   switch(x) {
         
    case 1: 
-        printf("enter message to encrypt:\n");
+        printf("enter message to encrypt:\n"); 
         scanf("%s\n",message);
-        printf("enter a key:\n");
+        printf("enter a key:\n"); //enter amount of shifts required in rotation
         scanf("%d", &shift);
-        E_rotation (message, shift);
+        E_rotation (message, shift); 
         
     case 2: 
         printf("enter message to decrypt:\n");
@@ -38,10 +38,10 @@ void E_rotation (char message[], int shift) {
   int i = 0;
 
   while (message[i] != '\0') {
-    if ((message[i] + shift) >= 65 && (message[i] + shift) <= 90) {
-      message[i] += (shift);
+    if ((message[i] + shift) >= A && (message[i] + shift) <= Z) { //ASCII value must be between A and Z
+      message[i] += (shift); //the ascii character is now equal to its value plus the shift
     } else {
-      message[i] += (shift - 25); 
+      message[i] += (shift - 25); //if the value plus the shifty exceeds Z, then minus 25 to bring the number to the begining of the alphabet
     }
     i++;
   }
